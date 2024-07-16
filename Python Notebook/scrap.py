59e5ddb6ff7c0d04b16d3dfbda31aee5a31940e5
Grace Wang <grace.wang.2@stonybrook.edu>
2024-07-16 19:45:03 -0400

    Lab 7 complete

--- Python Notebook/scrap.py
+++ Python Notebook/scrap.py
@@ -1,12 +1,27 @@
-def idk(self, nums):
+class Solution:
+    def countTeams (self, teamSize_1, teamSize_2, p):
+        maxCount = p // teamSize_1
+        team1Count = maxCount
+        # now replace teams with ppl from team2
+        remainder = (p % teamSize_1 != 0)
+        print(remainder)
+        currentCount = maxCount * teamSize_1
+        
+        while (currentCount >= 0):
+            team1Count -= 1
+            currentCount -= teamSize_1
+            newCount = (p - currentCount) // teamSize_2
+            if ((p - currentCount) % teamSize_2 == 0):
+                remainder = False
+            maxCount = max(maxCount, team1Count + newCount)
+        
+        if not remainder:
+            return maxCount
+        else: return -1
 
-    # maybe if the rightmost number isn't greater than the current one, then we have to look at other subarray
+def main():
+    solution = Solution()
 
-    left = 0
-    right = len(nums) - 1
+    print(solution.countTeams(2, 4, 7))
 
-    while (left <= right):
-        mid = (left + right) // 2
-        if nums[mid+1 % len(nums)-1] < nums[mid]: return nums[mid+1 % len(nums)-1]
-        if nums[mid] > nums[right]: left = mid + 1
-        else: right = mid - 1
+main()
